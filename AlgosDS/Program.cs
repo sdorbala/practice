2e6dbc7e91e36d6c3df9a0bd48c4088c0cf24941
Sarath Dorbala <sarath.dorbala@gmail.com>
2024-09-09 18:37:17 -0700

    Gas stations and circuits (partial code)

--- AlgosDS/Program.cs
+++ AlgosDS/Program.cs
@@ -1,7 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using AlgosDS.LeetCode;
+
 Console.WriteLine("Hello, World!");
 
-string invalid = "90.";
+string invalid = "-90.";
 
 Console.WriteLine($"Is this valid? {float.Parse(invalid)}");
+
+int[] xCoords = [2, 3, 2, 4, 2];
+int[] yCoords = [2, 2, 6, 5, 8];
+
+Console.WriteLine($"MaxPath = {FoodDrop.GetMaxLength(xCoords, yCoords)}.");
+
+xCoords.Rotate(2);
+xCoords.Print();
+yCoords.Rotate(3);
+yCoords.Print();
+
+int[] partiallySorted = [9,-3,-2,-1,0,1,2,3,4,5,6,7,8,];
+Console.Write("Partially Sorted Array: ");
+Arrays.Print(partiallySorted);
+Console.WriteLine($"Lowest in partially sorted array = {Arrays.FindMin(partiallySorted)}");
